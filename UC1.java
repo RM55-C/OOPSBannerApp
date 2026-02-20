601bdf621f3d804b92f0ee82b290e30edf432d35
RM55-C <rijutamondal55@gmail.com>
2026-02-20 21:34:06 +0530

    Add files via upload

---
+++ UC1.java
@@ -0,0 +1,9 @@
+// Define the class
+public class UC1 {
+
+    // Entry point of the application
+    public static void main(String[] args) {
+        // Print "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
